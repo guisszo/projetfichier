819fb202f79d11d705209ff93f873c4c7ff4e2a5
guisszo <leguisse@gmail.com>
2019-06-29 00:52:29 +0000

    modifications

--- EtudiantApp/pages/Ajoutchambre.php
+++ EtudiantApp/pages/Ajoutchambre.php
@@ -18,7 +18,7 @@
     require "../autoload/Autoloader.php";
     Autoloader::autoreg();
     include "navbar.php";
-   
+
 
     //die(var_dump($id));
 
@@ -33,21 +33,18 @@
                     <!-- <input type="text" name="chambre" placeholder="Num chambre"> -->
 
                     <select name="bati" id="bati">
-                    <option value=""></option>
-                    <?php
-                        $servi = new EtudiantService();
-                        $sc=$servi->findAll('batiment');
-                        
+                        <option value=""></option>
+                            <?php
+                            $servi = new EtudiantService();
+                            $sc = $servi->findAll('batiment');
+
                             foreach ($sc as $done) {
 
-                                $nomBati=$done->nom_bati;
-                                echo "<option value='".$done->id_bati."'>".$done->nom_bati." </option>";
-     
-                             }
-                        
-                        
-                    ?>
-                </select>
+                                $nomBati = $done->nom_bati;
+                                echo "<option value='" . $done->id_bati . "'>" . $done->nom_bati . " </option>";
+                            }
+                            ?>
+                    </select>
                     <input type="submit" name="ajouter" value="ajouter">
                 </form>
             </div>
